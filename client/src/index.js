40e4cdb7713c6cffa3d17cf3da8560ee325f89b8
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-05 10:49:28 -0500

    add dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
-import indexRoutes from "./routes/index.jsx";
+import indexRoutes from './routes/index';
 
-import "./assets/scss/material-kit-react.css?v=1.3.0";
+import './assets/scss/material-kit-react.css?v=1.3.0';
 
 var hist = createBrowserHistory();
 
